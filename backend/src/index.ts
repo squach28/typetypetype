7c5067c60f85f7cbabcf0d376d2b1ab317b05a8b
squach28 <squach3443@gmail.com>
2024-04-11 15:20:44 -0700

    generate random words using monkeytype

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import axios from 'axios'
 
 const PORT = process.env.PORT || 2000
 
@@ -21,12 +22,34 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'hello' })
 })
 
+const generatePassage = async () => {
+    const res = await axios.get('https://monkeytype.com/languages/english.json')
+    const passage = []
+    const words = res.data.words
+    const NUM_OF_WORDS = 100
+    for(let i = 0; i < NUM_OF_WORDS; i++) {
+        const word = words[Math.floor(Math.random() * words.length)]
+        passage.push(`${word} `)
+    }
+
+    return passage
+}
+
 io.on('connection', (socket) => {
     const animal = animals[Math.floor(Math.random() * animals.length)]
     mapping.set(socket.id, animal)
     socket.broadcast.emit('join', animal)
 
-    socket.on('message', (...args) => {
+    console.log(io.engine.clientsCount)
+
+    if(io.engine.clientsCount === 3) {
+        generatePassage()
+            .then((passage: string[]) => {
+                io.emit('send_passage', passage)
+            })
+    }
+
+    socket.on('message', async (...args) => {
         const msg = args[0]
         console.log(msg)
         const user = mapping.get(msg.userId)
@@ -38,6 +61,8 @@ io.on('connection', (socket) => {
         mapping.delete(socket.id)
         socket.broadcast.emit('left', animal)
     })
+
+    
 })
 
 server.listen(PORT, () => {
