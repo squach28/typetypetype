f6d0a38fa6a55d68a4f60bcc1ccfd22fcaceccc9
squach28 <squach3443@gmail.com>
2024-04-11 13:39:19 -0700

    add cors for socket server for development

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,29 @@
 import express, { Request, Response } from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 2000
 
 const app = express()
+const server = http.createServer(app)
+
+const mapping = {}
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:8080'
+    }
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ message: 'hello' })
 })
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log(socket)
+    socket.broadcast.emit('join', 'hello!')
+})
+
+server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
