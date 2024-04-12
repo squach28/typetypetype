4fe6da36d64f1a9f2bfa84c12192b79d8090711f
squach28 <squach3443@gmail.com>
2024-04-12 14:32:51 -0700

    fixed bug where toggling chat would reset connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,41 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Chat from "./components/Chat"
 import TypingTest from "./components/TypingTest"
 import ToggleChat from "./components/ToggleChat"
 import { useSearchParams } from 'react-router-dom'
-import { io } from "socket.io-client"
+import { Socket, io } from "socket.io-client"
 
 const App = () => {
   const [showChat, setShowChat] = useState<boolean>(true)
+  const [searchParams,] = useSearchParams()
+  const [socket, setSocket] = useState<Socket | null>(null)
+  const roomId = searchParams.get('roomId')
+
+  useEffect(() => {
+    const connectToRoom = (roomId: string | null) => {
+      if(roomId !== null) {
+        const SOCKET_URL = import.meta.env.NODE_ENV === 'production' ? '' : `http://localhost:2000?roomId=${roomId}`
+
+        const connection = roomId !== null ? io(SOCKET_URL, {
+          query: {
+            roomId
+          }
+        }) : null
+    
+        setSocket(connection)
+      }
+
+    }
+
+    connectToRoom(roomId)
+
+  }, [roomId])
+
+
+
   const toggleShowChat = () => {
     setShowChat(prev => !prev)
   }
-
-  const [searchParams,] = useSearchParams()
-  const roomId = searchParams.get('roomId')
-
-  const SOCKET_URL = import.meta.env.NODE_ENV === 'production' ? '' : `http://localhost:2000?roomId=${roomId}`
-
-  const socket = roomId !== null ? io(SOCKET_URL, {
-    query: {
-      roomId
-    }
-  }) : null
 
   return (
     roomId && socket !== null ? 
