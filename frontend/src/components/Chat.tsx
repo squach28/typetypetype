8e426c255951d9c646f230adbdcbb67a66950625
squach28 <squach3443@gmail.com>
2024-04-12 09:43:03 -0700

    memoize changes to chat history and render based on MessageType

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,23 +1,60 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { socket } from "../utils/socket"
+import { Message, MessageType } from "../types/Message"
 
 const Chat = () => {
-    const [chat, setChat] = useState<string[]>([])
+    const [chat, setChat] = useState<Message[]>([])
     const [message, setMessage] = useState<string>('')
+    const messages = useMemo(() => {
+        const renderMessage = (message: Message) => {
+            switch(message.type) {
+                case MessageType.MESSAGE:
+                    return (
+                        <li key={message.id}>
+                            <span>{message.nickname}: </span>
+                            <span>{message.content}</span>
+                        </li>
+                    )
+                case MessageType.JOIN:
+                    return(
+                        <li key={message.id}>
+                            <p>{message.nickname} joined the room!</p>
+                        </li>
+                    )
+                case MessageType.LEAVE:
+                    return(
+                        <li key={message.id}>
+                            <p>{message.nickname} left the room.</p>
+                        </li>
+                    )
+                default:
+                    return null
+            }
+        }
+
+        const renderMessages = () => {
+            return(
+                chat.map(message => 
+                   renderMessage(message)
+                )
+            )
+    
+        }
+        return renderMessages()
+    }, [chat])
+
     useEffect(() => {
-        socket.on('join', (...args: string[]) => {
-            const user = args[0]
-            const message = `${user} joined the room!`
+        socket.on('join', (...args: Message[]) => {
+            const message = args[0]
             setChat(prev => [...prev, message])
         })
 
-        socket.on('left', (...args: string[]) => {
-            const user = args[0]
-            const message = `${user} left the room.`
+        socket.on('left', (...args: Message[]) => {
+            const message = args[0]
             setChat(prev => [...prev, message])
         })
 
-        socket.on('message', (...args: string[]) => {
+        socket.on('message', (...args: Message[]) => {
             const message = args[0]
             setChat(prev => [...prev, message])
         })
@@ -31,7 +68,7 @@ const Chat = () => {
         e.preventDefault()
         if(message !== '') {
             socket.emit('message', {
-                userId: socket.id,
+                socketId: socket.id,
                 content: message
             })
             setMessage('')
@@ -41,12 +78,8 @@ const Chat = () => {
     return (
         <div className="flex flex-col bg-gray-800 w-1/5 h-full absolute right-0 top-0 p-4">
             <h1 className="text-white text-2xl">Chat</h1>
-            <ul>
-                {chat.map(msg => (
-                    <li key={msg}>
-                        {msg}
-                    </li>
-                ))}
+            <ul className="">
+                {messages}
             </ul>
             <form className="w-full mt-auto" onSubmit={onMessageSubmit}>
                 <input
