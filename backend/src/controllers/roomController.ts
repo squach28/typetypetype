59cf5553c10aa8deac7c87a44fdbdfc39d4b6b46
squach28 <squach3443@gmail.com>
2024-04-23 15:02:55 -0700

    hash room password and store in db

--- backend/src/controllers/roomController.ts
+++ backend/src/controllers/roomController.ts
@@ -1,16 +1,25 @@
 import { Request, Response } from "express";
 import { adminFirestore } from "../utils/db";
+import bcrypt from 'bcrypt'
 
 const COLLECTION_NAME = 'rooms'
+const SALT_ROUNDS = 10
 
 export const createRoom = async (req: Request, res: Response) => {
     try {
         const { name, password } = req.body
-        const doc = adminFirestore.collection(COLLECTION_NAME).doc().create({
+        const doc = adminFirestore.collection(COLLECTION_NAME).doc()
+        const hash = await bcrypt
+            .genSalt(SALT_ROUNDS)
+            .then(salt => bcrypt.hash(password, salt))
+            
+        await doc.create({
             name,
-            password
+            password: hash
         })
-        res.status(201).json({})
+        res.status(201).json({
+            id: doc.id,
+        })
     } catch(e) {
         res.status(500).json({ message: 'Something went wrong, please try again.'})
     }
