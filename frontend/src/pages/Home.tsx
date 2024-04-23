fe8dab01eb3ea2b3398bd4bc5f1f9bc8b180db92
squach28 <squach3443@gmail.com>
2024-04-23 11:08:53 -0700

    add buttons on home page for create/join room

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,15 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="bg-gray-700 text-white min-h-screen">
+      <h1>typetypetype</h1> 
+      <div className="flex flex-col justify-center items-center gap-4 my-8">
+        <p className="text-2xl font-bold">Compete with your friends to see who truly is the typing master</p>
+        <div className="flex gap-8">
+          <button className="border p-2 m-1 text-2xl">Create Room</button>
+          <button className="border p-2 m-1 text-2xl">Join Room</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
